c606bbaaa7cc169966cf7b697d24734f4e713382
Anshuman <anshuman@ucsb.edu>
2019-12-11 23:47:24 -0800

    Figuring out why x1 x2 y1 y2 are weird also hi rezi

--- imageManipulation.py
+++ imageManipulation.py
@@ -7,16 +7,21 @@ class imageRec:
 
         bbox = (x1, x2, y1, y2)
 
-        img = ImageGrab.grab(bbox=(x1,x2,y1,y2)) #BGR not RGB
+        img = ImageGrab.grab(bbox) #BGR not RGB
         img_np = np.array(img)
         frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
 
+        print("{}, {}  {}, {}".format(x1, x2, y1, y2))
+
         left = 0
         right = x2-x1
-        yVal = y1+y2
+        yVal = y1-y2
         yVal /= 2
 
-        
+
+        #for i in frame[int(yVal)]:
+            #print(i[1], end = ", ")
+        print()
 
         cv2.imshow("hpbar", frame)
         cv2.moveWindow("hpbar",3160,650)
