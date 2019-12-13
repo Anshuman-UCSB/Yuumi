b6d88d33e1e161e71195456d88337eaa2ba335fc
Anshuman <anshuman@ucsb.edu>
2019-12-12 17:33:35 -0800

    Changed binary sort, and stopped excessive healing outputs

--- imageManipulation.py
+++ imageManipulation.py
@@ -4,6 +4,11 @@ from PIL import ImageGrab
 import pyautogui
 
 class imageRec:
+
+    def __init__(self):
+        self.old_hp = -1
+        self.health = 100
+
     def getHealthValue(self, x1, x2, y1, y2):
 
         bbox = (x1, y1, x2, y2)
@@ -21,23 +26,33 @@ class imageRec:
         yVal = y1-y2
         yVal /= 2
 
+
+
         mid = -1
-        while(right-left>=1):
+        while(right-left>0):
             mid = int((right+left)/2)
             if(frame[int(yVal)][int(mid)][1]>120):
                 left = mid+1
             else:
                 right = mid-1
             #print("left {}, right {}, mid{}".format(left,right,mid))
-        print("Health is {0:.2f}%".format(100*mid/len(frame[0])))
+        if(abs(100*mid/len(frame[0])-self.health)>1):
+            #print("old {}, new {}".format(self.old_hp, self.health))
+            self.old_hp = self.health
+            self.health = 100*mid/len(frame[0])
+            print("Health is {0:.2f}%".format(self.health))
+
 
         upper = 60
         lower = 5
 
-        if 100*mid/len(frame[0]) < upper and 100*mid/len(frame[0]) > lower:
+        viewOnly = False
 
-            pyautogui.click(x=1205,y=983)
-            print("Healing, health is at {}".format(100*mid/len(frame[0])))
+        if viewOnly == False:
+            if self.health < upper and self.health > lower:
+
+                pyautogui.click(x=1205,y=983)
+                print("Healing, health is at {}".format(self.health))
 
 
         cv2.imshow("hpbar", frame)
